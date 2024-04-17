9577b07300dc86b0ca9a86a81168c09799ed0e6b
root <honggangwang1979@gmail.com>
2024-04-17 04:01:23 +0000

    add some benchmark fds: half/quater.fds

--- GPU/RTX4000/HPC/run_fds.sh
+++ GPU/RTX4000/HPC/run_fds.sh
@@ -1,18 +1,28 @@
-ln -sf ../../../bin/fds_gpu ./fds
+#cp /notebooks/firemodels/fds/Build/ompi_gnu_linux_db_CUDA/*CUDA ./fds
+cp /notebooks/firemodels/fds/Build/ompi_gnu_linux_CUDA/*CUDA ./fds
 
 date
 #time mpirun --allow-run-as-root -n 1 ./fds ./32a.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./64a_quater.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./64a_half.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./64a.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./128a_quater.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./128a_half.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./128a.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./256a_quater.fds
+time mpirun --allow-run-as-root -n 1 ./fds ./256a_half.fds
+time mpirun --allow-run-as-root -n 1 ./fds ./256a.fds
 #time mpirun --allow-run-as-root -n 1 ./fds ./64a.fds
 #time mpirun --allow-run-as-root -n 1 ./fds ./128a.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./256a.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./128a_half.fds
 #time mpirun --allow-run-as-root -n 1 ./fds ./HPC_32.fds
 #time mpirun --allow-run-as-root -n 1 ./fds ./HPC_64.fds
 #time mpirun --allow-run-as-root -n 1 ./fds ./HPC_128.fds
 #time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_36.fds
 #time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_72.fds
 #time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_144.fds
-
-date
+#
+#date
 #time mpirun --allow-run-as-root -n 1 ./fds ./32a_NR.fds
 #time mpirun --allow-run-as-root -n 1 ./fds ./64a_NR.fds
 #time mpirun --allow-run-as-root -n 1 ./fds ./128a_NR.fds
@@ -22,7 +32,7 @@ date
 #time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_36_NR.fds
 #time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_72_NR.fds
 #time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_144_NR.fds
-
+#
 #date
 #time mpirun --allow-run-as-root -n 2 ./fds ./32a_2m.fds
 #time mpirun --allow-run-as-root -n 2 ./fds ./64a_2m.fds
@@ -43,5 +53,10 @@ date
 #time mpirun --allow-run-as-root -n 4 ./fds ./HPC_128_4m.fds
 #time mpirun --allow-run-as-root -n 4 ./fds ./simple_test_36_4m.fds
 #time mpirun --allow-run-as-root -n 4 ./fds ./simple_test_72_4m.fds
-#time mpirun --allow-run-as-root -n 4 ./fds ./simple_test_144m.fds
+#time mpirun --allow-run-as-root -n 4 ./fds ./simple_test_144_4m.fds
 #date
+
+
+#mpirun --allow-run-as-root -n 1 nsys profile ./fds ./*32a.fds
+#nsys stats *rep >fds.report
+
--- NOMP/RTX4000/HPC/run_fds.sh
+++ NOMP/RTX4000/HPC/run_fds.sh
@@ -1,26 +1,38 @@
-ln -sf  ../../../bin/fds_nomp ./fds
+cp /notebooks/firemodels/fds/Build/ompi_gnu_linux_nomp/*nomp ./fds
 
 date
-time mpirun --allow-run-as-root -n 1 ./fds ./32a.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./64a.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./128a.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./HPC_32.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./HPC_64.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./HPC_128.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_36.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_72.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_144.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./32a.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./64a_quater.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./64a_half.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./64a.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./128a_quater.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./128a_half.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./128a.fds
+time mpirun --allow-run-as-root -n 1 ./fds ./256a_quater.fds
+time mpirun --allow-run-as-root -n 1 ./fds ./256a_half.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./256a.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./256a.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./128a_NR.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./256a_NR.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./128a_half.fds
+#time mpirun --allow-run-as-root -n 2 ./fds ./128a_2m.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./HPC_32.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./HPC_64.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./HPC_128.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_36.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_72.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_144.fds
 
-date
-time mpirun --allow-run-as-root -n 1 ./fds ./32a_NR.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./64a_NR.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./128a_NR.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./HPC_32_NR.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./HPC_64_NR.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./HPC_128_NR.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_36_NR.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_72_NR.fds
-time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_144_NR.fds
+#date
+#time mpirun --allow-run-as-root -n 1 ./fds ./32a_NR.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./64a_NR.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./128a_NR.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./HPC_32_NR.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./HPC_64_NR.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./HPC_128_NR.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_36_NR.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_72_NR.fds
+#time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_144_NR.fds
 
 #date
 #time mpirun --allow-run-as-root -n 2 ./fds ./32a_2m.fds
@@ -44,3 +56,8 @@ time mpirun --allow-run-as-root -n 1 ./fds ./simple_test_144_NR.fds
 #time mpirun --allow-run-as-root -n 4 ./fds ./simple_test_72_4m.fds
 #time mpirun --allow-run-as-root -n 4 ./fds ./simple_test_144m.fds
 #date
+
+
+#mpirun --allow-run-as-root -n 1 nsys profile ./fds ./*32a.fds
+#nsys stats *rep >fds.report
+
